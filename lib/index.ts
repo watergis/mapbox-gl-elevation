fbcc8c34dae2fbdb402d1816fa8c21a236e52e06
Jin IGARASHI <wushilan0623@gmail.com>
2021-01-17 14:11:09 +0900

    fixed a bug when style.json was replaced.

--- lib/index.ts
+++ lib/index.ts
@@ -213,15 +213,21 @@ export default class MapboxElevationControl implements IControl
     }
 
     clearFeatures(){
-      this.map?.removeLayer(LAYER_LINE);
-      this.map?.removeLayer(LAYER_SYMBOL);
-      this.map?.removeSource(SOURCE_LINE);
-      this.map?.removeSource(SOURCE_SYMBOL);
+      if (this.map){
+        if (this.map.getLayer(LAYER_LINE)) this.map.removeLayer(LAYER_LINE);
+        if (this.map.getLayer(LAYER_SYMBOL)) this.map.removeLayer(LAYER_SYMBOL);
+        if (this.map.getSource(SOURCE_LINE)) this.map.removeSource(SOURCE_LINE);
+        if (this.map.getSource(SOURCE_SYMBOL)) this.map.removeSource(SOURCE_SYMBOL);
+      }
       this.markers.forEach((m) => m.remove());
     }
 
     mapClickListener(event){
       const this_ = this;
+      if (!this_.map?.getSource(SOURCE_LINE) || !this_.map?.getSource(SOURCE_SYMBOL)){
+        this.clearFeatures();
+        this_.initFeatures();
+      }
       let zoom = this.map?.getZoom();
       if (!zoom) {
         zoom = 15;
